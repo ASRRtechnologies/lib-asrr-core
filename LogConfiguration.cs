07bdb0ffcbd5881dca2b10836212b8c4d6fe65da
Amar <armd@axi.nl>
2022-06-24 12:17:41 +0200

    Add files

---
+++ LogConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace ASRR.Log
+{
+    public class NLogBasedLogConfiguration
+    {
+        public bool OpenOnStartUp { get; set; }
+        public bool OpenOnButton { get; set; }
+        public string LogName { get; set; }
+        public string LogFilePath { get; set; }
+        public string LogLayout { get; set; }
+        public string NameFilter { get; set; }
+        public string MinLevel { get; set; }
+    }
+}
