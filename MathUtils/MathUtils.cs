e82f958970ba24db348959c77154d5d6ef5dec78
Amar <armd@axi.nl>
2022-11-11 17:22:03 +0100

    Add mathUtils

---
+++ MathUtils/MathUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ASRR.Core.MathUtils
+{
+    public class MathUtils
+    {
+        public static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+
+        public static double RadiansToDegrees(double radians)
+        {
+            return radians * 180 / Math.PI;
+        }
+
+
+        public static bool ApproximateEquals(double one, double two, double epsilon = 0.05)
+        {
+            return Math.Abs(one - two) < epsilon;
+        }
+    }
+}
