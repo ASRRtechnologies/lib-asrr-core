d661ae4f75b980713371d43d1dad3f09c7402d81
Amar Ramdas <amar.ramdas@asrr.nl>
2022-12-29 11:51:42 +0100

    Clean up using rider

--- Persistence/IPersistentStorageProvider.cs
+++ Persistence/IPersistentStorageProvider.cs
@@ -3,7 +3,7 @@ namespace ASRR.Core.Persistence
     public interface IPersistentStorageProvider
     {
         T Fetch<T>() where T : class;
-        
+
         bool Persist<T>(T toStore) where T : class;
     }
 }
--- Persistence/JsonBasedPersistenceProvider.cs
+++ Persistence/JsonBasedPersistenceProvider.cs
@@ -9,7 +9,7 @@ namespace ASRR.Core.Persistence
     public class JsonBasedPersistenceProvider : IPersistentStorageProvider
     {
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
-        
+
         private const string ProgramDataPath = @"C:\ProgramData\ASRR\Storage";
         private readonly string _path;
 
@@ -18,18 +18,18 @@ namespace ASRR.Core.Persistence
             _path = Path.Combine(ProgramDataPath, directory);
             Directory.CreateDirectory(_path);
         }
-        
+
         public T Fetch<T>() where T : class
         {
-            var filePath =Path.Combine(_path, FileName<T>());
-            if (!File.Exists(filePath)) 
+            var filePath = Path.Combine(_path, FileName<T>());
+            if (!File.Exists(filePath))
                 throw new FileNotFoundException($"File at path '{filePath}' does not exist");
 
             var deserializedObject = JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath));
 
             if (HasNullProperties(deserializedObject))
                 Log.Warn($"File at path '{filePath}' contains null properties");
-            
+
             return deserializedObject;
         }
 
@@ -42,7 +42,7 @@ namespace ASRR.Core.Persistence
         }
 
         private string FilePath<T>() where T : class
-        {            
+        {
             return Path.Combine(_path, $"{FileName<T>()}.json");
         }
 
@@ -50,7 +50,7 @@ namespace ASRR.Core.Persistence
         {
             return nameof(T);
         }
-        
+
         private bool HasNullProperties<T>(T obj)
         {
             var type = obj.GetType();
