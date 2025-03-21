23ae21ef24d1f5845626bb003115fea60551b76a
Amar97 <amar.ramdas@asrr.nl>
2025-03-21 14:19:55 +0100

    fix(settings): add machine-level and process-level fallbacks

--- Persistence/JsonBasedPersistenceProvider.cs
+++ Persistence/JsonBasedPersistenceProvider.cs
@@ -97,9 +97,10 @@ namespace ASRR.Core.Persistence
                         property.PropertyType == typeof(decimal))
                     {
                         var envVarName = ConvertToCamelCaseUpper(property.Name);
-                        var envVarValue = Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User 
-                                    
-                                );
+                        var envVarValue = Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.User) ?? 
+                            Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.Machine) ??
+                            Environment.GetEnvironmentVariable(envVarName, EnvironmentVariableTarget.Process)
+                      ;
                         Log.Info($"Checking environment variable '{envVarName}' for property '{property.Name}'");
                         Log.Info($"Value: {envVarValue}");
 
